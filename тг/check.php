8c4fe0256c7ec011efa73d79b4ea145fcefa5565
makskala <farget.farget1@gmail.com>
2025-04-10 08:05:30 +0300

    Delete тг directory

--- тг/check.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = __DIR__.'/test_data.txt';
-if(file_put_contents($file, "Тест запису ".date('H:i:s')) {
-    echo "Файл створений: ".realpath($file);
-    unlink($file); // Видалити тестовий файл
-} else {
-    echo "Помилка запису. Перевірте права на папку: ".__DIR__;
-}
-?>
